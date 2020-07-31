d8a00ebed6f3b6259cbec52e81f83d63352dfc82
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-31 11:23:32 +0700

    Set up View Pager Adapter

--- app/src/main/java/com/rujirakongsomran/viewpager2/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/viewpager2/MainActivity.java
@@ -1,14 +1,51 @@
 package com.rujirakongsomran.viewpager2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout parentView;
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        viewPager2 = (ViewPager2) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabDots);
+        parentView = (RelativeLayout) findViewById(R.id.parentView);
+
+        // Set Data
+        viewPager2.setAdapter(new MyViewPagerAdapter(this));
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                // Here you can set something for tab like set text...etc
+
+            }
+        }).attach();
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                Snackbar.make(parentView, "You have selected page: " + (position + 1), Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
